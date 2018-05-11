38354a92dbb4bfc7923d62921c718d270cf8d49b
adrianoadum <adumpsy@gmail.com>
2018-05-11 17:58:56 -0300

    create Pricing model

--- callcontrol/models.py
+++ callcontrol/models.py
@@ -40,3 +40,11 @@ class PhoneCallRecord(models.Model):
     call = models.ForeignKey(PhoneCall, on_delete=models.PROTECT)
     type = models.CharField(max_length=5, choices=RECORD_TYPES)
     timestamp = models.DateTimeField()
+
+
+class Pricing(models.Model):
+    name = models.CharField(max_length=255)
+    period_start = models.TimeField()
+    period_end = models.TimeField()
+    standing_price = models.DecimalField(max_digits=5, decimal_places=2)
+    price_per_minute = models.DecimalField(max_digits=5, decimal_places=2)
